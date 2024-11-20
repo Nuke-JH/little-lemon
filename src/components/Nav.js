b6e49cc07bb2d6cf4c64cce4279680c382c451bc
JAH <179285424+Nuke-JH@users.noreply.github.com>
2024-11-19 21:15:59 -0800

    At the start of Module 2

---
+++ src/components/Nav.js
@@ -0,0 +1,29 @@
+function Nav() {
+  return (
+    <nav className="main-nav">
+      <img src="LittleLemon.jpg" alt="Logo" />
+      <ul>
+        <li>
+          <a href="index.html">Home</a>
+        </li>
+        <li>
+          <a href="index.html">About</a>
+        </li>
+        <li>
+          <a href="Menu.html">Menu</a>
+        </li>
+        <li>
+          <a href="index.html">Reservations</a>
+        </li>
+        <li>
+          <a href="index.html">Order Online</a>
+        </li>
+        <li>
+          <a href="index.html">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
